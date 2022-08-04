1feadc8b2082119e3867c3001939cba8d175df26
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-04 08:47:09 -0300

    first commit

---
+++ src/main/java/com/ericsson/APIposts/ApiPostsApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.APIposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericsson/APIposts/ApiPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.APIposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
