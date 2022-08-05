52986caedabe0f11bc2d3cad50ee96f6404cb3ae
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-05 08:46:11 -0300

    Inserção de usuário com POST

--- src/main/java/com/ericsson/APIposts/services/UserService.java
+++ src/main/java/com/ericsson/APIposts/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ericsson.APIposts.services;
 
 import com.ericsson.APIposts.domain.User;
+import com.ericsson.APIposts.dto.UserDTO;
 import com.ericsson.APIposts.repository.UserRepository;
 import com.ericsson.APIposts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,17 @@ public class UserService {
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO onjDto) {
+        return new User(onjDto.getId(), onjDto.getName(), onjDto.getEmail());
     }
 }
