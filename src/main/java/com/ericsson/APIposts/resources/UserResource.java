be00b70e6281a5839d8521edc34d7299c7c00f00
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-04 23:13:09 -0300

    Using DTO pattern to return users

--- src/main/java/com/ericsson/APIposts/resources/UserResource.java
+++ src/main/java/com/ericsson/APIposts/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ericsson.APIposts.resources;
 
 import com.ericsson.APIposts.domain.User;
+import com.ericsson.APIposts.dto.UserDTO;
 import com.ericsson.APIposts.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +18,11 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
