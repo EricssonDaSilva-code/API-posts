4573b533a8e75c5a5397919dc9c683cae1b31707
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-04 23:43:27 -0300

    Getting a user by id

--- src/main/java/com/ericsson/APIposts/services/UserService.java
+++ src/main/java/com/ericsson/APIposts/services/UserService.java
@@ -2,10 +2,12 @@ package com.ericsson.APIposts.services;
 
 import com.ericsson.APIposts.domain.User;
 import com.ericsson.APIposts.repository.UserRepository;
+import com.ericsson.APIposts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,9 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
