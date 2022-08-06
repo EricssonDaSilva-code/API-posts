f3959d0a9840916e83a17ad5210d8f73d2f9b309
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-06 08:58:30 -0300

    Simple query with query methods

--- src/main/java/com/ericsson/APIposts/resources/PostResource.java
+++ src/main/java/com/ericsson/APIposts/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.ericsson.APIposts.resources;
 
 import com.ericsson.APIposts.domain.Post;
+import com.ericsson.APIposts.resources.util.URL;
 import com.ericsson.APIposts.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +22,11 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+
+    }
 }
--- src/main/java/com/ericsson/APIposts/services/PostService.java
+++ src/main/java/com/ericsson/APIposts/services/PostService.java
@@ -6,6 +6,7 @@ import com.ericsson.APIposts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,11 +15,12 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-
     public Post findById(String id) {
         Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
 }
