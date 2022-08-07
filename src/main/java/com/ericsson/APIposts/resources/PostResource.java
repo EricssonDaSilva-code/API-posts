af419c349f56dead5214e44fd2878b0896c458b3
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-06 22:08:33 -0300

    Query with multiple criteria

--- src/main/java/com/ericsson/APIposts/resources/PostResource.java
+++ src/main/java/com/ericsson/APIposts/resources/PostResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -29,4 +30,18 @@ public class PostResource {
         return ResponseEntity.ok().body(list);
 
     }
+
+    @GetMapping("/fullsearch")
+            public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0));
+        Date max = URL.convertDate(maxDate, new Date(0));
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+
+    }
+
 }
